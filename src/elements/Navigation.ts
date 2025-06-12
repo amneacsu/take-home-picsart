48128d1b721689a70c02321c5e0d0e1746232efe
Andrei Neacșu <amneacsu@gmail.com>
2025-06-12 17:49:13 +0300

    feat: add search box and page title

---
+++ src/elements/Navigation.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Navigation = styled.nav`
+  position: sticky;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  top: 0;
+  height: 72px;
+  background-color: #fff;
+  box-shadow: 0 0 20px #00000040;
+`;
